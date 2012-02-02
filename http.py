21e00c785daed5b416ead11e8c5c01bd289dba0e
Mark Everett <mark.everett@rackspace.com>
2012-02-02 10:49:53 -0800

    made github web hooks send a sexy message

--- http.py
+++ http.py
@@ -1,6 +1,7 @@
 from twisted.words.xish import domish
 from twisted.web import resource
 from twisted.python import log
+import json
 
 class XmppResource(resource.Resource):
     def __init__(self, xmpp_prot):
@@ -12,14 +13,27 @@ class GithubResource(XmppResource):
         return "got it!"
 
     def render_POST(self, request):
-        post_data = request.content.read()
+        post_data = json.loads(request.args['payload'][0])
         log.msg("GITHUB POST: %s" % post_data)
+
+        pusher = post_data['pusher']['name']
+        repo = post_data['repository']['name']
+        date = post_data['repository']['pushed_at']
+        commits = [(c['message'], c['url']) for c in post_data['commits']]
+        commit_links = '\n'.join(['"{0}"\n<{1}>'.format(*commit) for commit in commits])
+        message = ("{pusher} just pushed to {repo} on "
+                  "{date}\n\n{commits}").format(pusher=pusher,
+                                                 repo=repo,
+                                                 date=date,
+                                                 commits=commit_links)
+
+        log.msg("ABOUT TO SEND: %s" % message)
 
         reply = domish.Element((None, "message"))
         reply["to"] = "test2@chat.mynnx.com/Marks-MacBook-Air"
         reply["from"] = "test@chat.mynnx.com/twisted"
         reply["type"] = 'chat'
-        reply.addElement("body", content=post_data)
+        reply.addElement("body", content=message)
         self.xmpp_prot.send(reply)
         return ''
 
